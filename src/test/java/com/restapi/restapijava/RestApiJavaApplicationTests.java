9c054475b857ddd98450b352ecd7c4a615fcf49c
Manelito <emanuelwork1@gmail.com>
2023-03-15 15:13:51 -0300

    feat: create maven project

---
+++ src/test/java/com/restapi/restapijava/RestApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restapi.restapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
